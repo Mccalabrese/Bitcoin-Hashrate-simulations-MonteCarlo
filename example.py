05bbd5b0a91be62caf1e878b445b99a7d511a118
gouldrussell <144621362+gouldrussell@users.noreply.github.com>
2023-11-02 20:32:19 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,17 @@
 #Data Analysis Flow
 
-#Import APIs/CSV with prior hash rate history by country and market (3 years)
+#Import CSV with prior hash rate history by country and market (3 years)
 
-#Clean data and createdData frames with rates of change (daily/monthly)
+#Clean data and created data frames with absoulte rates of change (daily/monthly)
 
 #High level statistical summaries with variance / stdev
 
 #Plot data to visualize country overlay of hash rates (PyViz)
 
-#Perform monte carlo simulations for prediction of future hash rates (initial investments  = energy?)
+#Identify top three countries with prior market share (year / year) 
+
+#Perform monte carlo simulations for prediction of future hash rates
+
+#Identify top three countries with most signficant growth (month/month) 
+
+#Identify when US market will exceed 50% of global market share with confidence interval
