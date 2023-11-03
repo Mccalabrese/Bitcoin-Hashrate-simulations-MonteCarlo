ba3ad9f28412ef70f8c30cd2c3e9e5eef5958a2c
gouldrussell <144621362+gouldrussell@users.noreply.github.com>
2023-11-02 20:27:28 -0400

    Update example.py

---
+++ example.py
@@ -0,0 +1,11 @@
+#Data Analysis Flow
+
+#Import APIs/CSV with prior hash rate history by country and market (3 years)
+
+#Clean data and createdData frames with rates of change (daily/monthly)
+
+#High level statistical summaries with variance / stdev
+
+#Plot data to visualize country overlay of hash rates (PyViz)
+
+#Perform monte carlo simulations for prediction of future hash rates (initial investments  = energy?)
